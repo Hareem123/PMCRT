fa5e9d82afe2663c28fb1d9c14427161d4594a18
hareem123 <harim36148@gmail.com>
2020-04-09 14:53:50 +0500

    changing project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to StreetLight-KE!');
+    expect(page.getTitleText()).toEqual('Welcome to PMCRT!');
   });
 
   afterEach(async () => {
